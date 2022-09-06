be7acba2f57b6cb034f82101f2d3c2fe74f036b6
unknown <redaattamahmoud@gmail.com>
2022-09-06 21:09:25 +0200

    first commit

---
+++ toDO_API/ToDo_API2/Controllers/Todoitems.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDo_API2.Models;
+
+namespace ToDo_API2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Todoitems : ControllerBase
+    {
+        List<Item> Items = new List<Item>();
+        private IEnumerable<object> items;
+
+        public Todoitems()
+        {
+            Items.Add(new Item
+            {
+                id = 1,
+                name = "mahmoud",
+                Iscomplete = true
+
+            });
+            Items.Add(new Item
+            {
+                id = 2,
+                name = "reda",
+                Iscomplete = false
+
+            });
+        }
+        [HttpGet]
+        public List<Item> Get()
+        {
+            return Items;
+        }
+        // api/items/1   or    api/items/1
+        [HttpGet("{ip}")]
+        public ActionResult<Item> GetById(int id)
+        {
+            var itm = Items.FirstOrDefault(a => a.id == id);
+            if (itm == null)
+                return NotFound();
+            else
+                return itm;
+        }
+        [HttpPost]
+        public ActionResult Post([FromBody] Item item)
+        {
+            Items.Add(item);
+            return CreatedAtAction("GetById", new { id = item.id }, item);
+        }
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, Item item)
+        {
+            foreach (var x in items)
+            {
+                if (item.id == id)
+                {
+                    item.name = item.name;
+                }
+            }
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public ActionResult DeleteToDoItem(int id)
+        {
+            var xitem = Items.Find(a => a.id == id);
+            if (xitem == null)
+            {
+                return NotFound();
+
+            }
+            Items.Remove(xitem);
+            return NoContent();
+
+        }
+    
+
+    }
+}
---
+++ toDO_API/ToDo_API2/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo_API2.Models
+{
+    public class Item
+    {
+        [Required]
+        [Range(1,100)]
+        public int id { get; set; }
+        [StringLength(maximumLength:9, MinimumLength =2)]
+        public string name { get; set; }
+        public bool Iscomplete { get; set; }
+    }
+}
